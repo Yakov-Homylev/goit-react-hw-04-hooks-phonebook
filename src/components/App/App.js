d9f164528c4f748f682dadee404b5045d72340c3
Yakov <yakoventii@gmail.com>
2022-01-03 18:15:58 +0200

    Add hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,44 +1,42 @@
 import { nanoid } from "nanoid";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import ContactForm from "../ContactForm/ContactForm";
 import Filter from "../Filter/Filter";
 import ContactList from "../ContactList/ContactList";
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-  };
+function App() {
+  const [contacts, setContacts] = useState([
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]);
+  const [filter, setFilter] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     const notParsedContacts = localStorage.getItem("contacts");
-    const contacts = JSON.parse(notParsedContacts);
-    if (contacts) {
-      this.setState({ contacts });
+    if (!notParsedContacts) {
+      return;
     }
-  }
+    const localContacts = JSON.parse(notParsedContacts);
+    setContacts([...localContacts]);
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
-  onSubmitForm = (e) => {
+  const addContacts = (e) => {
     e.preventDefault();
+
     const name = e.currentTarget.elements.name.value;
     const number = e.currentTarget.elements.number.value;
     const id = nanoid();
 
-    const isNameIncludeInArray = this.state.contacts.find(
+    const isNameIncludeInArray = contacts.find(
       (contact) => contact.name === name
     );
-    const isNumberIncludeInArray = this.state.contacts.find(
+    const isNumberIncludeInArray = contacts.find(
       (contact) => contact.number === number
     );
     if (isNameIncludeInArray) {
@@ -49,49 +47,36 @@ class App extends Component {
       alert(`${number} is already in contacts.`);
       return;
     }
+    setContacts([...contacts, { name, number, id }]);
 
-    this.setState({ contacts: [...this.state.contacts, { name, id, number }] });
     e.currentTarget.reset();
   };
 
-  searchByName = (e) => {
-    this.setState({ filter: e.currentTarget.value.toLowerCase() });
+  const removeContact = (id) => {
+    const newContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts([...newContacts]);
   };
 
-  filteringArray = () => {
-    return this.state.contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(this.state.filter)
+  const findByName = (e) => {
+    setFilter(e.target.value.toLowerCase());
+  };
+
+  const filteredArray = () => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter)
     );
   };
 
-  removeContactById = (id) => {
-    const removedArray = this.state.contacts.filter(
-      (contact) => contact.id !== id
-    );
-    this.setState({ contacts: removedArray });
-  };
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmitForm={addContacts} />
 
-  render() {
-    const contactsArray = this.filteringArray();
-
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmitForm={this.onSubmitForm} />
-
-        <h2>Contacts</h2>
-        <Filter
-          type="text"
-          title="Find contacts by name"
-          onChange={this.searchByName}
-        />
-        <ContactList
-          array={contactsArray}
-          removeContact={this.removeContactById}
-        />
-      </div>
-    );
-  }
+      <h2>Contacts</h2>
+      <Filter title="Find contacts by name" onChange={findByName} />
+      <ContactList array={filteredArray()} removeContact={removeContact} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Section, Title, FilterInput } from "./Filter.styled";
 
-export default class Filter extends Component {
-  render() {
-    return (
-      <Section>
-        <Title>{this.props.title}</Title>
-        <FilterInput type="text" onChange={this.props.onChange} />
-      </Section>
-    );
-  }
+export default function Filter({ title, onChange }) {
+  return (
+    <Section>
+      <Title>{title}</Title>
+      <FilterInput type="text" onChange={onChange} />
+    </Section>
+  );
 }
 
-Filter.prototyp = {
+Filter.propTypes = {
   title: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
